f823874671a64bc18abe6805920a33ebd515487e
biscotty666 <biscotty666@gmail.com>
2024-12-22 10:43:55 -0700

    Initial commit;

---
+++ flake.nix
@@ -0,0 +1,71 @@
+{
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShell = pkgs.mkShell {
+        name = "python-venv";
+        venvDir = "./.venv";
+        buildInputs = with pkgs; [
+          (python3.withPackages(ps: with ps; [
+            ipython
+            pip
+            jupyter
+            widgetsnbextension
+            ipympl
+            jupyter-nbextensions-configurator
+            jedi-language-server
+            osmnx
+            ipywidgets
+            mypy
+            pandas
+            numpy
+            geopandas
+            geodatasets
+            pyogrio
+            geopy
+            matplotlib
+            pyproj
+            folium
+            mapclassify
+            scipy
+            shapely
+            xarray
+            rioxarray
+            rasterio
+          ]))
+        ];
+        postVenvCreation = ''
+          unset SOURCE_DATE_EPOCH
+          pip install -r requirements.txt
+        '';
+
+        shellHook = ''
+            export BROWSER=floorp
+                # Tells pip to put packages into $PIP_PREFIX instead of the usual locations.
+    # See https://pip.pypa.io/en/stable/user_guide/#environment-variables.
+            export PIP_PREFIX=$(pwd)/venvDir
+            export PYTHONPATH="$PIP_PREFIX/${pkgs.python3.sitePackages}:$PYTHONPATH"
+            export PATH="$PIP_PREFIX/bin:$PATH"
+            unset SOURCE_DATE_EPOCH
+            #jupyter lab
+        '';
+
+        postShellHook = ''
+          # allow pip to install wheels
+          unset SOURCE_DATE_EPOCH
+        '';
+      };
+    }
+  );
+}
