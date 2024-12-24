d48d05287e756a96a315a05c5fa283486ce1290a
biscotty666 <biscotty666@gmail.com>
2024-12-24 11:05:29 -0700

    Added points of interest;

--- docs/utils/Globals.py
+++ docs/utils/Globals.py
@@ -1,5 +1,5 @@
 import geopandas as gpd
 
 DATA_PATH = "../../data/"
-abq_shp = gpd.read_file(DATA_PATH + "local/abq_base.shp")
-vtr_shp = gpd.read_file(DATA_PATH + "shp/vtr_shp.gpkg")
+abq_shp = gpd.read_file(DATA_PATH + "local/abq_shp.gpkg")
+vtr_shp = gpd.read_file(DATA_PATH + "local/vtr_shp.gpkg")
