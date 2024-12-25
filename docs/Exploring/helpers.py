ee314a8549c94e83fd78f1afe01bf6edb200e234
biscotty666 <biscotty666@gmail.com>
2024-12-25 16:08:00 -0700

    Refactor; Cleaned up Housing;

--- docs/Exploring/helpers.py
+++ docs/Exploring/helpers.py
@@ -1,4 +1,5 @@
 import geopandas as gpd
+import pandas as pd
 from shapely.geometry import box, Point
 from contextily import Place
 import contextily as ctx
@@ -31,3 +32,48 @@ def reg_plot(data, column, scheme, title,
     plt.ylim(ylim[0], ylim[1])
     plt.title(title, fontsize=20)
     ctx.add_basemap(ax, crs=data.crs, source=ctx.providers.Esri.WorldStreetMap)
+
+def unit_summary():
+    housing_nm = gpd.read_file("../../data/local/housing_nm.gpkg")
+    housing_abq = gpd.read_file("../../data/local/housing_abq.gpkg")
+    housing_vtr = gpd.read_file("../../data/local/housing_vtr.gpkg")
+    
+    unit_summary = pd.DataFrame()
+    unit_summary['Location'] = ['New Mexico', 'Albuquerque', 'VTR']
+
+    unit_summary['Total'] = [
+        housing_nm.HOUSING_UN.sum(), 
+    housing_abq.HOUSING_UN.sum(), 
+    housing_vtr.HOUSING_UN.sum()
+    ] 
+    unit_summary['Owned'] = [
+        housing_nm.OWNED.sum(), 
+        housing_abq.OWNED.sum(), 
+        housing_vtr.OWNED.sum()
+    ]
+    unit_summary['Rented'] = [
+        housing_nm.RENTED.sum(), 
+        housing_abq.RENTED.sum(), 
+        housing_vtr.RENTED.sum()
+    ]
+    unit_summary['Occupied'] = [
+        housing_nm.OCC_HU.sum(), 
+        housing_abq.OCC_HU.sum(), 
+        housing_vtr.OCC_HU.sum()
+    ]
+    unit_summary['Vacant'] = [
+        housing_nm.VAC_HU.sum(), 
+        housing_abq.VAC_HU.sum(), 
+        housing_vtr.VAC_HU.sum()
+    ]
+    unit_summary["Ownership_est"] = [
+        housing_nm.OWNED_P.mean(), 
+        housing_abq.OWNED_P.mean(), 
+        housing_vtr.OWNED_P.mean()
+    ]
+    unit_summary["Available"] = [
+        housing_nm.AVAILABILITY.mean(), 
+        housing_abq.AVAILABILITY.mean(), 
+        housing_vtr.AVAILABILITY.mean()
+    ]
+    return unit_summary
